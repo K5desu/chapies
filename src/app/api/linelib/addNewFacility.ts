b29022a7ed0e4eac5898387c0749fc1fc269d926
y220010@mail.ryukoku.ac.jp <y220010@mail.ryukoku.ac.jp>
2024-06-29 21:05:57 +0900

    ass

--- src/app/api/linelib/addNewFacility.ts
+++ src/app/api/linelib/addNewFacility.ts
@@ -3,8 +3,8 @@ import gemini from "@/lib/gemini";
 import * as line from "@line/bot-sdk";
 export async function addNewFacility(text: string) {
   const config = {
-    channelAccessToken: process.env.CHANNEL_TOKEN,
-    channelSecret: process.env.CHANNEL_SECRET,
+    channelAccessToken: process.env.CHANNEL_TOKEN || "",
+    channelSecret: process.env.CHANNEL_SECRET || "",
   };
   console.log("config", config);
   if (config.channelAccessToken && config.channelSecret) {
