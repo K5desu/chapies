419f3a9ec91d13896fef9b25107c91233acb82d3
y220010@mail.ryukoku.ac.jp <y220010@mail.ryukoku.ac.jp>
2024-07-13 14:50:28 +0900

    add

--- src/lib/geminiTagAdd.ts
+++ src/lib/geminiTagAdd.ts
@@ -10,7 +10,7 @@ const gemini = async (inputText: string) => {
   const model = genAI.getGenerativeModel({ model: "gemini-pro" });
   const result = await model.generateContent(
     `
-施設の提案をしてもらいます。以下の情報と例を元に提案してください。
+施設の提案をしてもらいます。以下の情報と例を元に提案してくださいませ。
 施設名：steamコモンズ
 説明：ものづくりができる場所。3Dプリンター、レーザーカッター、大型インクジェットプリンターなどを設置しており、（大型機械は予約必要）マイコンも貸し出ししているためIoTを用いたものづくりも経験できる。
 タグ：IT、文化部、サークル、部活、ボランティア
