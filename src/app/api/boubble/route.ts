ebc6e37c28d445a3d103dd9ae27846e973e597fb
y220010@mail.ryukoku.ac.jp <y220010@mail.ryukoku.ac.jp>
2024-07-13 13:50:03 +0900

    aa

--- src/app/api/boubble/route.ts
+++ src/app/api/boubble/route.ts
@@ -6,3 +6,9 @@ export async function GET(request: Request) {
   const response = await gemini(req);
   return Response.json({ response });
 }
+export async function POST(request: Request) {
+  const req = await request.json();
+  console.log(req);
+  const response = await gemini(req);
+  return Response.json({ response });
+}
