c47d75971593ca85a0e0043acfdc4a6422e2d422
y220010@mail.ryukoku.ac.jp <y220010@mail.ryukoku.ac.jp>
2024-08-06 20:23:40 +0900

    add

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -12,13 +12,39 @@ import { useToast } from "@/components/toast/use-toast";
 import { RyuAuthenticator } from "@/lib/ryu-authentcator";
 import Logout from "@/components/google/Logout";
 import { Button } from "@/components/ui/button";
+import Card from "@/components/ui/card";
 import NotRyuAlert from "@/components/google/notRyuAlert";
+
 export default function Page() {
+  const [imageUrl, setImageUrl] = useState("");
   const isRyu = RyuAuthenticator();
   const [articleUrl, setArticleUrl] = useState("");
   const { toast } = useToast();
-  const { title, content, setArticleContent } = useArticleStore();
+  const { title, content, setArticleContent, tag } = useArticleStore();
+
   const inputFileRef = useRef<HTMLInputElement>(null);
+  const handleImageChange = (e: any) => {
+    const file = e.target.files[0];
+    if (file) {
+      if (file.size > 4.5 * 1024 * 1024) {
+        // 500MBをバイトに変換
+        toast({
+          variant: "destructive",
+          title: "画像サイズが大きすぎます",
+          description: "ファイルサイズは4.5MB以下にしてください。",
+        });
+
+        return;
+      }
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        if (typeof reader.result === "string") {
+          setImageUrl(reader.result);
+        }
+      };
+      reader.readAsDataURL(file);
+    }
+  };
   async function handleSubmit() {
     if (!inputFileRef.current?.files) {
       throw new Error("No file selected");
@@ -47,7 +73,13 @@ export default function Page() {
             value={articleUrl}
             onChange={(e) => setArticleUrl(e.target.value)}
           ></Input>
-          <Input type="file" ref={inputFileRef} required />
+          <Input
+            type="file"
+            ref={inputFileRef}
+            required
+            placeholder="500mb以下の画像を選択してください"
+            onChange={handleImageChange}
+          />
           <div className="flex gap-x-4">
             <FacilityName action="create" />
             <FacilityTag action="create" />
@@ -59,6 +91,17 @@ export default function Page() {
             value={content}
             placeholder="Content"
           />
+          <div className="md:mx-auto">
+            <Card
+              title={title}
+              content={content}
+              tags={tag}
+              img={imageUrl}
+              url={articleUrl}
+              userid="1"
+              isRyu={true}
+            />
+          </div>
           <Button type="submit">upload</Button>
         </form>
       ) : (
