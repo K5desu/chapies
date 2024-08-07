72ebd40136cd96c32ea67a860c55399200cd3215
y220010@mail.ryukoku.ac.jp <y220010@mail.ryukoku.ac.jp>
2024-08-07 22:41:48 +0900

    add

--- src/lib/article/postLine.ts
+++ src/lib/article/postLine.ts
@@ -97,58 +97,8 @@ export default async function postLine(
           replyToken: action,
           messages: [
             {
-              type: "flex",
-              altText: "This is a Flex Message",
-              contents: {
-                type: "bubble",
-                hero: {
-                  type: "image",
-                  url: imgurl,
-                  size: "full",
-                  aspectRatio: "20:13",
-                  aspectMode: "cover",
-                  action: {
-                    type: "uri",
-                    uri: articleUrl,
-                  },
-                },
-                body: {
-                  type: "box",
-                  layout: "vertical",
-                  contents: [
-                    {
-                      type: "text",
-                      text: title,
-                      weight: "bold",
-                      size: "xl",
-                    },
-                  ],
-                },
-                footer: {
-                  type: "box",
-                  layout: "vertical",
-                  spacing: "sm",
-                  contents: [
-                    {
-                      type: "button",
-                      style: "link",
-                      height: "sm",
-                      action: {
-                        type: "uri",
-                        label: "Webサイトを見る",
-                        uri: websiteUrl,
-                      },
-                    },
-                    {
-                      type: "box",
-                      layout: "vertical",
-                      contents: [],
-                      margin: "sm",
-                    },
-                  ],
-                  flex: 0,
-                },
-              },
+              type: "text",
+              text: "新しい記事が投稿されました",
             },
           ],
         });
