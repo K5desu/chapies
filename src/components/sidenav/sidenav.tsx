472e75c7825a95832b07f7cf1b3fd7c69310f754
y220010@mail.ryukoku.ac.jp <y220010@mail.ryukoku.ac.jp>
2024-08-07 23:09:12 +0900

    add

--- src/components/sidenav/sidenav.tsx
+++ src/components/sidenav/sidenav.tsx
@@ -2,17 +2,31 @@
 import { usePathname } from "next/navigation";
 import Link from "next/link";
 import NavLinks from "@/components/sidenav/navlinks";
-
+import Image from "next/image";
 export default function SideNav() {
   const pathname = usePathname();
 
   return (
     <div className="flex h-full flex-col px-3 py-4 md:px-2 justify-between">
       <Link
-        className="mb-2 flex h-20 items-end justify-start rounded-md bg-gray-600 p-4 md:h-40"
+        className="mb-2 flex h-20 items-end justify-start rounded-md bg-sky-100 p-4 md:h-40"
         href="/"
       >
-        <div className="flex items-center h-full w-32 text-white md:w-40"></div>
+        <Image
+          src="/cover.jpg"
+          width={300}
+          height={120}
+          alt="cover"
+          className="w-full h-full md:hidden"
+        />
+
+        <Image
+          src="/icon.png"
+          width={140}
+          height={140}
+          alt="icon"
+          className="w-full h-full hidden md:block"
+        />
       </Link>
       <div className="flex grow flex-row space-x-2 md:flex-col md:space-x-0 md:space-y-2">
         <NavLinks />
